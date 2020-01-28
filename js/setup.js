e1660669a62e116ba4b4844cdd754b72cc0ef3d5
tomhell27 <53575983+tomhell27@users.noreply.github.com>
2020-01-28 19:42:43 +0300

    Убираем магическое число

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,7 @@ var WIZARD_NAMES = [' Иван', 'Хуан Себастьян', 'Мария', '�
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-
+var WIZARDS_NUMBER = 4;
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
@@ -27,7 +27,7 @@ var createWizard = function (length) {
   return arr;
 };
 
-var wizards = createWizard(4);
+var wizards = createWizard(WIZARDS_NUMBER);
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
